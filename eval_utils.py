79164fd1078d7a7637fe0336e9af6b68de50075e
Vrishank Chandrasekhar <109028447+vrishankc@users.noreply.github.com>
2024-02-21 20:25:23 -0800

    Create eval_utils.py

---
+++ eval_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+def bootstrap_auc(y_true, y_pred, n_bootstraps=2000, rng_seed=42):
+    n_bootstraps = n_bootstraps
+    rng_seed = rng_seed
+    bootstrapped_scores = []
+
+    rng = np.random.RandomState(rng_seed)
+    for i in range(n_bootstraps):
+        indices = rng.randint(len(y_pred), size=len(y_pred))
+        score = roc_auc_score(y_true[indices], y_pred[indices])
+        bootstrapped_scores.append(score)
+    bootstrapped_scores = np.array(bootstrapped_scores)
+
+    print("AUROC: {:0.3f}".format(roc_auc_score(y_true, y_pred)))
+    print("Confidence interval for the AUROC score: [{:0.3f} - {:0.3}]".format(
+        np.percentile(bootstrapped_scores, (2.5, 97.5))[0], np.percentile(bootstrapped_scores, (2.5, 97.5))[1]))
+    
+    return roc_auc_score(y_true, y_pred), np.percentile(bootstrapped_scores, (2.5, 97.5))
